f5cfa18f3ce659d91b000e9bda22381dc93864f3
quocanhne123 <orchidftu@gmail.com>
2026-07-08 20:28:33 +0700

    pls work:(

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,8 @@
-FDEVICE="duchamp"
+export ALLOW_MISSING_DEPENDENCIES=true
 
-fox_get_target_device() {
-    local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+export FOX_BUILD_DEVICE=duchamp
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
 
-    if [ -n "$chkdev" ]; then
-        FOX_BUILD_DEVICE=$FDEVICE
-    else
-        FOX_BUILD_DEVICE=$(get_build_var FOX_BUILD_DEVICE)
-    fi
-}
-
-fox_get_target_device
-
-if [ "$1" = "$FDEVICE" ] || [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-    export ALLOW_MISSING_DEPENDENCIES=true
-    export FOX_BUILD_DEVICE=duchamp
-    export FOX_AB_DEVICE=1
-    export FOX_VIRTUAL_AB_DEVICE=1
-    export OF_DEFAULT_KEYMASTER_VERSION=4.1
-    export OF_FORCE_PREBUILT_KERNEL=1
-fi
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
+export OF_FORCE_PREBUILT_KERNEL=1
